af45b866e76e7e725e9fe37027564e0a8a4fc664
waruna <waruna@binarylogs.com>
2025-01-09 10:17:43 +0530

    fix a type

--- lib/geo_ppp.rb
+++ lib/geo_ppp.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "geo_ppp/version"
-require_relative "geo_app/fetcher"
+require_relative "geo_ppp/fetcher"
 
 module GeoPpp
   class Error < StandardError; end
